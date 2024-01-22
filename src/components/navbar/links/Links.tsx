9919ea07ed2250bd4e0c7a8926738769627acdb3
Maksymilian Walicki <mwalicki@student.agh.edu.pl>
2024-01-22 13:32:38 +0100

    Setup for burger menu

--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,34 +1,59 @@
+"use client";
+
+import { useState } from "react";
 import "./links.css";
 import NavLink from "./navLink/navLink";
 
 // Links
-const Links: React.FC = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 
+const Links: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  // TODO: TEMP
   const session = true;
+  const isAdmin = true;
 
   return (
-    <div className="links">
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
+    <div className="container">
+      <div className="links">
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}{" "}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className="logout">Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button onClick={() => setOpen((prev) => !prev)}>menu</button>
+      {open && (
+        <div className="mobileLinks">
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
